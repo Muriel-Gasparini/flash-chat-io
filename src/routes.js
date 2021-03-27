4c30f491259e00953f942150ec378f2e0c5c6748
Muriel Gasparini <muriel.developer@gmail.com>
2021-03-27 10:15:19 -0300

    change routes and fiz link in home page

--- src/routes.js
+++ src/routes.js
@@ -3,27 +3,25 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 
 import { HomePage } from './pages/Home/Home'
 
-
 function getRoute(path, component) {
   return (
     <Route path={path}>
       { component }
     </Route>
-  )  
+  )
 }
 
-function getRoutes () {
+function getRoutes() {
   return (
     <Router>
       <Switch>
-        { ALL_ROUTES }
+        {[ALL_ROUTES, getRoute('/', HomePage)]}
       </Switch>
     </Router>
   )
 }
 
 const ALL_ROUTES = [
-  getRoute('/', HomePage)
 ]
 
 export { getRoutes }
