7d1f9487d60a7d325999dc4013651b2d9a9ca17b
Tiago Lustosa Santos <tiagolustosasantos@hotmail.com>
2021-04-04 08:18:53 -0300

    Create ChatUserMessage component

---
+++ src/components/ChatUserMessage/ChatUserMessage.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import { Card, Col, Image, ListGroup, Row } from 'react-bootstrap'
+import UserProfileImage from '../../images/icon-256x256.png'
+
+function ChatUserMessage({ userName, message }) {
+  <ListGroup.Item bsPrefix="borderless p-4">
+    <Row>
+      <Col md={1} className="d-flex flex-column align-items-center">
+        <Image src={UserProfileImage} roundedCircle width={50} height={50} />
+        <h6>{userName}</h6>
+      </Col>
+      <Col md={2}>
+        <Card style={{ width: '48rem' }} bg="secondary" text="light">
+          <Card.Text className="p-2">
+            {message}
+          </Card.Text>
+        </Card>
+      </Col>
+    </Row>
+  </ListGroup.Item>
+}
+export { ChatUserMessage }
