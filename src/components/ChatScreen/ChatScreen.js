d6504f1283fb39f9e6e5377de84a24e8b473e186
Tiago Lustosa Santos <tiagolustosasantos@hotmail.com>
2021-04-04 11:24:27 -0300

    Testing ChatScreen with data

--- src/components/ChatScreen/ChatScreen.js
+++ src/components/ChatScreen/ChatScreen.js
@@ -1,13 +1,82 @@
 import { Jumbotron, InputGroup, FormControl, Button, ListGroup } from 'react-bootstrap'
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { Page } from '../page/page'
+import { ChatAnonymousMessage } from '../ChatAnonymousMessage/ChatAnonymousMessage'
+import { ChatUserMessage } from '../ChatUserMessage/ChatUserMessage'
+
+
+function renderMessage(id, userName, messageType, message) {
+  switch (messageType) {
+    case 'anonymous':
+      return <ChatAnonymousMessage key={id} message={message} />
+    case 'user':
+      return <ChatUserMessage key={id} userName={userName} message={message} />
+  }
+}
 
 
 function ChatScreen() {
+  const [data, setData] = useState([
+    {
+      userName: 'Teste',
+      messageType: 'user',
+      message: 'dsalkdsaklkldsakldsaklsdalkklsdalksdalkkldsal'
+    },
+    {
+      userName: 'Teste',
+      messageType: 'anonymous',
+      message: 'dsalkdsaklkldsakldsaklsdalkklsdalksdalkkldsal'
+    },
+    {
+      userName: 'Teste',
+      messageType: 'user',
+      message: 'dsalkdsaklkldsakldsaklsdalkklsdalksdalkkldsal'
+    },
+    {
+      userName: 'Teste',
+      messageType: 'user',
+      message: 'dsalkdsaklkldsakldsaklsdalkklsdalksdalkkldsal'
+    },
+    {
+      userName: 'Teste',
+      messageType: 'anonymous',
+      message: 'dsalkdsaklkldsakldsaklsdalkklsdalksdalkkldsal'
+    },
+    {
+      userName: 'Teste',
+      messageType: 'user',
+      message: 'dsalkdsaklkldsakldsaklsdalkklsdalksdalkkldsal'
+    },
+    {
+      userName: 'Teste',
+      messageType: 'anonymous',
+      message: 'dsalkdsaklkldsakldsaklsdalkklsdalksdalkkldsal'
+    },
+    {
+      userName: 'Teste',
+      messageType: 'user',
+      message: 'dsalkdsaklkldsakldsaklsdalkklsdalksdalkkldsal'
+    },
+    {
+      userName: 'Teste',
+      messageType: 'user',
+      message: 'dsalkdsaklkldsakldsaklsdalkklsdalksdalkkldsal'
+    }
+
+  ])
+
+  useEffect(() => {
+    setInterval(() => {
+      setData(data.concat({ userName: 'Teste', messageType: 'user', message: 'klsdalkdaslkadskldaslklkdsa' }))
+    }, 5000)
+  }, [])
   return (
     <Page title="FlashChat">
       <Jumbotron style={{ height: '70vh', overflowY: 'scroll' }}>
         <ListGroup>
+          {data.map((msg, i) => {
+            return renderMessage(i.id, msg.userName, msg.messageType, msg.message)
+          })}
         </ListGroup>
       </Jumbotron>
       <InputGroup className="mb-3">
@@ -16,7 +85,6 @@ function ChatScreen() {
       </InputGroup>
     </Page>
   )
-
 }
 
 export { ChatScreen }
