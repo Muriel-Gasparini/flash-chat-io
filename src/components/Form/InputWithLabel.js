ea28e449203e55caac77bc7f9372cac21f71c0b1
lermen12 <lermenlucas@outlook.com>
2022-01-16 20:53:51 -0300

    add custom classname for input with label component

--- src/components/Form/InputWithLabel.js
+++ src/components/Form/InputWithLabel.js
@@ -1,12 +1,12 @@
 import React from 'react'
 import { FormGroup, FormLabel } from 'react-bootstrap'
 
-function InputWithLabel({ label, placeholder, onChange, inputType, required, min, max }) {
+function InputWithLabel({ label, placeholder, onChange, inputType, required, min, max, className }) {
   return (
     <FormGroup>
       <FormLabel>{label}</FormLabel>
       <input
-        className="form-control"
+        className={className || "form-control"}
         placeholder={placeholder}
         type={inputType}
         onChange={onChange}
