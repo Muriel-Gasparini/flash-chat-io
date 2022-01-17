0adcf8c0afb1d6c937b2061e29bca13f6de9fba6
lermen12 <lermenlucas@outlook.com>
2022-01-16 21:15:28 -0300

    add get and set for JSON in localstorage

--- src/Services/LocalStorageService.js
+++ src/Services/LocalStorageService.js
@@ -3,6 +3,14 @@ class LocalStorage {
   static setItem(name, item) {
     localStorage.setItem(name, item)
   }
+  
+  static setJSONItem(name, item) {
+    localStorage.setItem(name, JSON.stringify(item))
+  }
+
+  static getJSONItem(name) {
+    return JSON.parse(localStorage.getItem(name))
+  }
 
   static getItem(name) {
     return localStorage.getItem(name)
